7f4308551b6de96a5b04cc00c0491ebcbc2b9c1c
nerofor <gergosoos4@gmail.com>
2016-11-28 13:19:26 +0100

    A header és a footer rész hozzáadva. A css fájlt is hozzáadtam, de még nincs benne semmi.

---
+++ web_support/protected/footer.php
@@ -0,0 +1,4 @@
+	</div>
+	<footer>
+		<p class="footer">Készítette: navController @ 2016</p>
+	</footer>
---
+++ web_support/protected/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="hu">
+	<title>omniBuszProject 1.0</title>
+		<meta charset='UTF-8'>
+		<meta name="author" content="navController Kft.">
+		<meta name="keywords" content="játék, windows, gyerekjáték, flash, 3D, pegi 3+">
+		<meta name="description" content="3D-s játék, minden korosztály számára." >
+		
+		
+		<link href='../public/css/default.css' type='text/css' rel='stylesheet'>
+</head>
+<body>
+<header>
+	<h1><a href="/index.php">omniBuszProject 1.0</a></h1>
+</header>
+<div class="container">
