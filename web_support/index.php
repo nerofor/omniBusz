e6f44e62a7ca15772019dc201378bbdbac342fad
nerofor <gergosoos4@gmail.com>
2016-11-28 12:41:19 +0100

    Webes felület mapparendszere. De ez még csak a kezdetleges.

---
+++ web_support/index.php
@@ -0,0 +1,7 @@
+<?php
+	require_once('protected/functions.php');
+	require_once('protected/header.php');
+	require_once ('protected/aside.php');
+	require_once ('protected/content.php');
+	require_once('protected/footer.php')
+?>
