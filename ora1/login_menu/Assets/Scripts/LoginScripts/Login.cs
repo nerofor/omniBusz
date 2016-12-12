cb61b9a9109a62cf43858b66a0608089b1f8373b
nerofor <gergosoos4@gmail.com>
2016-12-12 16:05:28 +0100

    A bejelentkezési menü. Csak bejelentkezni lehet. Akkor tudd  akedves felhasználó bejelentkezni ha már előtt regisztrált az oldalon,

---
+++ ora1/login_menu/Assets/Scripts/LoginScripts/Login.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : MonoBehaviour {
+    #region Variables
+    //static variables
+    public static string Emai = "";
+    public static string Password = "";
+
+
+    //private variables
+    private string LoginUrl = "";
+
+
+    //public variables
+    public string CurrentMenu = "Login";
+
+    //GUI test section
+    public float X;
+    public float Y;
+    public float Width;
+    public float Height;
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void onGUI() {
+        if(CurrentMenu == "Login")
+        {
+            LoginGUI();
+        }    
+    }
+
+    #region
+    void LoginGUI()
+    {
+        GUI.Box(new Rect(X,Y,Width, Height),"");
+    }
+    #endregion
+}
