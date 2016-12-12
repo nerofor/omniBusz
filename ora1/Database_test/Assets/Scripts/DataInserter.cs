219d877ef3236408c4c0e7fd5be066e27b17954a
nerofor <gergosoos4@gmail.com>
2016-12-12 16:02:47 +0100

    AZ adatbázi kapcsolatot tudja letelsztelni, bejelentkezés esetén. Még nincs hozzáadva a projekthez, de már külön működik.

---
+++ ora1/Database_test/Assets/Scripts/DataInserter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataInserter : MonoBehaviour {
+
+    public string inputName;
+    public string inputPassword;
+    public string inputEmail;
+
+    string CreateUserURL = "localhost/omnibusz_database/InsertUser.php";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateUser(inputName, inputPassword,inputEmail);
+        }
+	
+	}
+
+    public void CreateUser(string username, string password, string email) {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("passwordPost", password);
+        form.AddField("emailPost", email);
+
+        WWW www = new WWW(CreateUserURL, form);
+    }
+}
---
+++ ora1/Database_test/Assets/Scripts/Login.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : MonoBehaviour {
+
+    public string inputUsername;
+    public string inputPassword;
+    string LoginURL = "localhost/omnibusz_database/Login.php";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StartCoroutine(LoginToDB(inputUsername, inputPassword));
+        }
+    }
+
+    IEnumerator LoginToDB(string username, string password) {
+        WWWForm form = new WWWForm();
+        form.AddField("usernamePost", username);
+        form.AddField("passwordPost", password);
+
+        WWW www = new WWW(LoginURL, form);
+
+        yield return www;
+
+        Debug.Log(www.text);
+
+    }
+}
